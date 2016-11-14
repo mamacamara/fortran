7ec1088566dac824c13df34a1ca3c01fb3bb8ccd
Mamadou  Camara <camara0388@gmail.com>
2016-11-14 12:02:16 +0100

    new

---
+++ src/constants.mod
@@ -0,0 +1,43 @@
+GFORTRAN module version '10' created from procedures.f90
+MD5:26d607e38ae1c850a5dee576e01776dc -- If you edit this, you'll get what you deserve.
+
+(() () () () () () () () () () () () () () () () () () () () () () () ()
+() () ())
+
+()
+
+()
+
+()
+
+()
+
+()
+
+(2 '__convert_i4_r4' '(intrinsic)' '' 1 ((PROCEDURE UNKNOWN-INTENT
+UNKNOWN-PROC UNKNOWN UNKNOWN 0 0 FUNCTION ELEMENTAL PURE) (REAL 4 0 0 0
+REAL ()) 0 0 () () 2 () () () 0 0)
+3 '__convert_r4_r8' '(intrinsic)' '' 1 ((PROCEDURE UNKNOWN-INTENT
+UNKNOWN-PROC UNKNOWN UNKNOWN 0 0 FUNCTION ELEMENTAL PURE) (REAL 8 0 0 0
+REAL ()) 0 0 () () 3 () () () 0 0)
+4 'atan' '(intrinsic)' '' 1 ((PROCEDURE UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN UNKNOWN 0 0 FUNCTION) (UNKNOWN 0 0 0 0 UNKNOWN ()) 0 0 () () 4 ()
+() () 0 0)
+5 'constants' 'constants' '' 1 ((MODULE UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN UNKNOWN 0 0) (UNKNOWN 0 0 0 0 UNKNOWN ()) 0 0 () () 0 () () () 0
+0)
+6 'kind' '(intrinsic)' '' 1 ((PROCEDURE UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN UNKNOWN 0 0 FUNCTION) (UNKNOWN 0 0 0 0 UNKNOWN ()) 0 0 () () 6 ()
+() () 0 0)
+7 'pi' 'constants' '' 1 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+EXPLICIT-SAVE 0 0) (REAL 8 0 0 0 REAL ()) 0 0 () () 0 () () () 0 0)
+8 'preci' 'constants' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () (
+CONSTANT (INTEGER 4 0 0 0 INTEGER ()) 0 '4') () 0 () () () 0 0)
+9 'precr' 'constants' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () (
+CONSTANT (INTEGER 4 0 0 0 INTEGER ()) 0 '8') () 0 () () () 0 0)
+)
+
+('__convert_i4_r4' 0 2 '__convert_r4_r8' 0 3 'atan' 0 4 'constants' 0 5
+'kind' 0 6 'pi' 0 7 'preci' 0 8 'precr' 0 9)
--- src/procedures.f90
+++ src/procedures.f90
@@ -1,32 +1,32 @@
 !module procedures
 module constants
-!	implicit none 
+	implicit none 
 	integer, parameter :: precI=kind(1), precR=kind(1.d0)
-	real ( kind = precR ) , save :: pi !Declaration statique de Pi 
+	real ( kind = precR ) , save :: pi = 4*atan(1.0) !Declaration statique de Pi 
 end module constants
 !	use constants 
 	! Densite de proba de la loi normale
 	! De moyenne mu et decart type sigma
-	function loi_normale(x,x_mu,sigma) result f 
+	function loi_normale(xx,x_mu,sigma) result(f)
 		use constants 
-!		implicit none
-		real ( kind = precR) :: x, sigma,x_mu,f
-		f = (1/(sigma*sqrt(2.*pi)))*exp(-(x-x_mu)**2/(2.*sigma**2))
+	!	implicit none
+		real(kind=precR) :: xx, sigma,x_mu,f
+		f = (1.0/(sigma*sqrt(2.*pi)))*exp(-(xx-x_mu)**2/(2.*sigma**2))
 	end function loi_normale
 	! Integration par la methode des rectangles	
 	! de loi_normale(,mu,sigma) sur [x_min;x_max] avec le pas h_step
 	! le resultat est mis sur result
 	subroutine methode_rect(loi_normale,sigma,x_mu,x_min,x_max,h_step,resultat)
 		use constants 
-!	 	implicit none  
-		real (kind = precR) ::  f0,fN
+	 !	implicit none  
+		real (kind = precR) ::  f0,fN,loi_normale,xi
 		real ( kind = precR), intent(in) :: x_min
 		real ( kind = precR), intent(in) :: x_max
 		real ( kind = precR), intent(in) :: h_step
 		real ( kind = precR), intent(out) :: resultat
-	        integer ( kind = precI) ::  N = (x_max-x_min)*h_step  ,i=1                                 
+	        integer ( kind = precI) ::  N   ,i=1                                 
         !	real ( kind = precR), intent(in) :: mu, sigma
-	!	N = (x_max-x_min)*h_step 
+		N = (x_max-x_min)*h_step 
 		f0 = loi_normale(x_min,x_mu,sigma)
 		fN = loi_normale(x_max,x_mu,sigma)
 		!i = 1
@@ -40,11 +40,11 @@ end module constants
 	end subroutine methode_rect   
 program mon_main 
 	use constants 
+	implicit none
 !	implicit none
-!	implicit none
-	real(kind = precR):: fonction
-	fonction = loi_normale(0.4,0.,1.)
-	write(*,*) 'La valeur de la loi normale centrée réduite au point 0.4 est' 	
+	real(kind = precR):: fonction,loi_normale
+	fonction = loi_normale(0.,0.,1.)
+	write(*,*) "La valeur de la loi normale centrée réduite au point 0. est"
 	write(*,*) fonction
 end program mon_main 
 
